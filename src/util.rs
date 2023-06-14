6a3b715873c9a83f0bda5542f465c61b0a49379d
co012 <co123@interia.eu>
2023-06-15 00:04:48 +0200

    TimePu

--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,7 @@
 use std::collections::HashMap;
+use std::marker::PhantomData;
+use ecrs::aco::pheromone::{Pheromone, PheromoneUpdate};
+use ecrs::aco::Solution;
 
 pub fn process_items(items: &[usize]) -> (usize, HashMap<usize, usize>, Vec<usize>) {
     let mut size_count = 1;
@@ -14,4 +17,23 @@ pub fn process_items(items: &[usize]) -> (usize, HashMap<usize, usize>, Vec<usiz
         }
     }
     (size_count, size_to_index, index_to_size)
+}
+
+pub struct TimedPU<Pher: Pheromone,P: PheromoneUpdate<Pher>> {
+    pu: P,
+    _phantom: PhantomData<Pher>
+}
+
+impl<Pher: Pheromone, P: PheromoneUpdate<Pher>> TimedPU<Pher, P> {
+    pub fn new(pu: P) -> Self {
+        Self { pu, _phantom: Default::default() }
+    }
+}
+
+
+impl<Pher: Pheromone,P: PheromoneUpdate<Pher>> PheromoneUpdate<Pher> for TimedPU<Pher, P> {
+    #[time_graph::instrument]
+    fn apply(&mut self, pheromone: &mut Pher, solutions: &[Solution], evaporation_rate: f64) {
+        self.pu.apply(pheromone, solutions,evaporation_rate)
+    }
 }
